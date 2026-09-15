f7c5268a1e2d4bd65c185aa370ae44a2d33e2601
yoann5930 <cardoria59330@gmail.com>
2026-09-16 01:03:42 +0200

    feat(ops): allow secure SumUp configuration command

--- oracle/cardoria-ops-ssh-wrapper.sh
+++ oracle/cardoria-ops-ssh-wrapper.sh
@@ -41,6 +41,9 @@ case "$cmd" in
   smtp-configure)
     exec sudo -n /usr/local/bin/cardoria-ops smtp-configure
     ;;
+  sumup-configure)
+    exec sudo -n /usr/bin/bash /opt/cardoria/current/oracle/sumup-configure.sh
+    ;;
   deploy)
     exec sudo -n /usr/local/bin/cardoria-ops deploy
     ;;
